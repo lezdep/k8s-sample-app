ed2bd6a7484451239139ab2ce01e062b4977d22c
Serguei Izvoztchikov <sizvoztchikov@ethoca.com>
2017-10-13 15:18:17 -0400

    New version to trigger pipeline

--- src/main/scala/org/lezdep/k8s/sample/Main.scala
+++ src/main/scala/org/lezdep/k8s/sample/Main.scala
@@ -3,7 +3,7 @@ package org.lezdep.k8s.sample
 object Main {
   def main(args: Array[String]): Unit = {
     while (true) {
-      println("Hello from K8S sample app")
+      println("Hello from K8S sample app deployed via Spinnaker pipeline")
       Thread.sleep(20000)
     }
   }
